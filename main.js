c3a2814f1bccf372923de6787040a01b1249fd02
RudhraHimalasya <79926737+RudhraHimalasya@users.noreply.github.com>
2022-06-23 19:47:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,11 @@
 leftEar = 0;
 rightEar = 0;
 
-
 function preload() {
     bunny_ears = loadImage("https://i.postimg.cc/9FPb5VHT/cute-easter-bunny-ears-digital-illustration-173599863-removebg-preview.png");
 }
-
-function draw(){
-    image(video,50,50,500,500)
-    image(bunny_ears , leftEar ,rightEar );
-   }
-
-function take() {
-    save("filter") 
-}
-
 function setup(){
-    canvas=createCanvas(420, 350);
+    canvas=createCanvas(800, 800);
 
     canvas.position(520,290);
     video=createCapture(VIDEO);
@@ -25,20 +14,26 @@ function setup(){
     poseNet = ml5.poseNet(video, modelloaded);
     poseNet.on('pose',gotposes)
 }
-
-
-        
+function modelloaded(){
+    console.log('posnet is initialized');
+}
 function gotposes(results){
     if(results.length>0)
     {
-        leftEar = results[0].pose.leftEar.x;
-        rightEar = results[0].pose.rightEar.y;
+        leftEar = results[0].pose.leftEar.x-150;
+        rightEar = results[0].pose.rightEar.y-300;
         console.log(results);
         console.log("ear x =" + results[0].pose.leftEar.x)
         console.log("ear y =" + results[0].pose.rightEar.y)
     }
 }
 
-function modelloaded(){
-    console.log('posnet is initialized');
+function draw(){
+    image(video,50,50,800,800)
+    image(bunny_ears , leftEar ,rightEar ,300,300);
+   }
+
+function take() {
+    save("filter") 
 }
+        
